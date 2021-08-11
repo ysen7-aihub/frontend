039b05402dd6274d043cee16918d60fbd46cb521
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-12 00:08:20 +0900

    Delete server directory

--- server/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = new express.Router();
-const image = [];
-router.post("/api", (req, res) => {
-  res.send("GET ok");
-});
-
-module.exports = router;
